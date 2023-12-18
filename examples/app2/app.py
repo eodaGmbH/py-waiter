eb5ab34388d1759062a89069375f73571c19358c
Stefan Kuethe <stefan.kuethe@eoda.de>
2023-12-18 14:59:04 +0100

    Old files

---
+++ examples/app2/app.py
@@ -0,0 +1,28 @@
+import time
+
+from shiny import App, render, ui
+
+from waiter import use_waiter, waiter_hide, waiter_show
+from waiter.spinners import spin_double_bounce
+
+app_ui = ui.page_fluid(
+    use_waiter(),
+    ui.panel_title("Hello Shiny!"),
+    ui.input_slider("n", "N", 0, 100, 20),
+    ui.output_text_verbatim("txt", placeholder=True),
+)
+
+
+def server(input, output, session):
+    @render.text
+    async def txt():
+        await waiter_show(html=spin_double_bounce())
+        time.sleep(5)
+        await waiter_hide()
+        return f"n*2 is {input.n() * 2}"
+
+
+app = App(app_ui, server)
+
+if __name__ == "__main__":
+    app.run()
