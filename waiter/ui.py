14ded8f7c47593c30d09d929032360f0189baae0
Nico Frieß <nico.friess@eoda.de>
2023-12-19 10:31:56 +0100

    add docstring

--- waiter/ui.py
+++ waiter/ui.py
@@ -2,6 +2,11 @@ from htmltools import HTMLDependency
 
 
 def use_waiter() -> HTMLDependency:
+    '''
+    Programatically show and hide loading screens.
+    Place this somewhere in the UI code of your shiny app.
+    
+    '''
     return HTMLDependency(
         name="waiter",
         version="1.0.0",
